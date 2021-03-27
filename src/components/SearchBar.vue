6421974f8ac61284c45ad06d8b8774e17153ddba
alperdaggez96 <alperdaggez96@gmail.com>
2021-03-27 11:52:33 +0300

    search feature

---
+++ src/components/SearchBar.vue
@@ -0,0 +1,118 @@
+<template>
+  <v-card
+    color="red lighten-2"
+    dark
+  >
+    <v-card-text>
+      <v-autocomplete
+        v-model="model"
+        :items="items"
+        :loading="isLoading"
+        :search-input.sync="search"
+        color="white"
+        hide-no-data
+        hide-selected
+        item-text="Description"
+        item-value="imdbID"
+        label="Public APIs"
+        placeholder="Start typing to Search"
+        prepend-icon="mdi-database-search"
+        return-object
+      ></v-autocomplete>
+    </v-card-text>
+    <v-divider></v-divider>
+    <v-expand-transition>
+      <v-list
+        v-if="model"
+        class="red lighten-3"
+      >
+        <v-list-item
+          v-for="(field, i) in fields"
+          :key="i"
+        >
+          <v-list-item-content>
+            <v-list-item-title v-text="field.value"></v-list-item-title>
+            <v-list-item-subtitle v-text="field.key"></v-list-item-subtitle>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-expand-transition>
+    <v-card-actions>
+      <v-spacer></v-spacer>
+      <v-btn
+        :disabled="!model"
+        color="grey darken-3"
+        @click="model = null"
+      >
+        Clear
+        <v-icon right>
+          mdi-close-circle
+        </v-icon>
+      </v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script>
+  export default {
+    data: () => ({
+      descriptionLimit: 60,
+      entries: [],
+      isLoading: false,
+      model: null,
+      search: null,
+    }),
+
+    computed: {
+      fields () {
+        if (!this.model) return []
+
+        return Object.keys(this.model).map(key => {
+          return {
+            key,
+            value: this.model[key] || 'n/a',
+          }
+        })
+      },
+      items () {
+        if (!this.entries) return []
+        return this.entries.map(entry => {
+          const Description = entry.Title.length > this.descriptionLimit
+            ? entry.Title.slice(0, this.descriptionLimit) + '...'
+            : entry.Title
+          return Object.assign({}, entry, { Description })
+        })
+      },
+    },
+
+    watch: {
+      search (val) {
+        // Items have already been loaded
+        //if (this.items.length > 0) return
+
+        // Items have already been requested
+        if (this.isLoading) return
+
+        this.isLoading = true
+
+        // Lazily load input items
+        fetch(`http://www.omdbapi.com/?apikey=be1732ea&s=${val}`)
+          .then(res => res.json())
+          .then(res => {
+            const { totalResults:count, Search:entries } = res
+            //const { count, entries } = res
+            this.count = count
+            this.entries = entries
+            console.log(res)
+          })
+          .catch(err => {
+            console.log(err)
+          })
+          .finally(() => (this.isLoading = false))
+      },
+    },
+  }
+</script>
+
+
+
