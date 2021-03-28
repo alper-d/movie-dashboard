635b3a848de323f666a8c7ba39ab57f4c014ece5
alperdaggez96 <alperdaggez96@gmail.com>
2021-03-28 21:01:59 +0300

    final commit

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -1,116 +1,197 @@
-<template>
-  <v-card
-    color="red lighten-2"
-    dark
-  >
-    <v-card-text>
-      <v-autocomplete
-        v-model="model"
-        :items="items"
-        :loading="isLoading"
-        :search-input.sync="search"
-        color="white"
-        hide-no-data
-        hide-selected
-        item-text="Description"
-        item-value="imdbID"
-        label="Public APIs"
-        placeholder="Start typing to Search"
-        prepend-icon="mdi-database-search"
-        return-object
-      ></v-autocomplete>
-    </v-card-text>
-    <v-divider></v-divider>
-    <v-expand-transition>
-      <v-list
-        v-if="model"
-        class="red lighten-3"
-      >
-        <v-list-item
-          v-for="(field, i) in fields"
-          :key="i"
-        >
-          <v-list-item-content>
-            <v-list-item-title v-text="field.value"></v-list-item-title>
-            <v-list-item-subtitle v-text="field.key"></v-list-item-subtitle>
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-    </v-expand-transition>
-    <v-card-actions>
-      <v-spacer></v-spacer>
-      <v-btn
-        :disabled="!model"
-        color="grey darken-3"
-        @click="model = null"
-      >
-        Clear
-        <v-icon right>
-          mdi-close-circle
-        </v-icon>
-      </v-btn>
-    </v-card-actions>
-  </v-card>
+<template lang="pug">
+  v-card(colo="red lighten-2" dark)
+    v-card-text
+        v-combobox(  
+            ref="combobox"  
+            v-model="model"
+            :items="items"
+            :loading="isLoading"
+            :search-input.sync="search"
+            color="white"
+            clearable
+            hide-no-data
+            item-text="Description"
+            item-value="imdbID"
+            label="Public APIs"
+            placeholder="Start typing to Search"
+            prepend-icon="mdi-database-search"
+            return-object
+            menu-props="{disableKeys: false}"
+            )
+    
+    v-divider
+    v-container(v-if="paginationLoading" fluid)
+        v-row(align="center" justify="center" class="my-1")
+            v-progress-circular( indeterminate color="red") 
+    v-expand-transition
+        v-container(v-if="detailBlock && details" fluid)
+            v-row(align="center" justify="center")
+                v-col(cols="12" sm="4" )
+                    v-img( :src="details.Poster" v-show="details.Poster")
+                v-col(cols="12" sm="8")
+                    v-list( class="blue-grey darken-2")
+                        v-list-item(v-for="(field, i) in fields" v-if="typeof(field.value) === 'string'" :key="i")
+                            v-list-item-content
+                                v-list-item-title(v-text="field.value")
+                                v-list-item-subtitle(v-text="field.key")
+
+    v-expand-transition
+        v-container(v-if="paginatedData" fluid)
+            v-pagination(v-model="page" :length="paginationLength" circle)
+            v-row
+                v-col(v-for="(item, i) in paginatedData" :key="i" cols="12" sm="2" md="3")
+                    
+                    v-card(class="m-1" height="100%" @click="() => cardClicked(item.imdbID, i)")
+                        v-card-title(class="justify-center text-wrap bottom" bottom="0%") {{item.Title}}
+                        v-expand-transition
+                            div(v-if="item.showDetail")
+                                v-img(:src="item.Poster" width="100%")
+                                v-card-subtitle(class="text-center text-wrap" ) {{item.Year}}
+                                
 </template>
 
 <script>
+
+import Vue from 'vue'
+//import goTo from 'vuetify/es5/services/goto'
+
   export default {
+    name: "SearchBar",
+
     data: () => ({
-      descriptionLimit: 60,
-      entries: [],
-      isLoading: false,
-      model: null,
-      search: null,
+        descriptionLimit: 60,
+        page:1,
+        paginationLength:1,
+        maxPerPage:10,
+        entries: [],
+        isLoading: false,
+        paginationLoading:false,
+        model: null,
+        search: null,
+        paginatedData:null,
+        focusedMovie:null,
+        details:null,
+        detailBlock: false,
     }),
 
     computed: {
       fields () {
-        if (!this.model) return []
+        if (!this.details) return []
 
-        return Object.keys(this.model).map(key => {
+        return Object.keys(this.details).map(key => {
           return {
             key,
-            value: this.model[key] || 'n/a',
+            value: this.details[key] || 'n/a',
           }
         })
       },
-      items () {
+        items () {
         if (!this.entries) return []
         return this.entries.map(entry => {
-          const Description = entry.Title.length > this.descriptionLimit
+            const Description = entry.Title.length > this.descriptionLimit
             ? entry.Title.slice(0, this.descriptionLimit) + '...'
             : entry.Title
-          return Object.assign({}, entry, { Description })
+            return Object.assign({}, entry, { Description })
         })
-      },
+        },
+    },
+    methods: {
+        searchQuery (changePagination) {
+            this.paginationLoading = true
+            const val= this.search
+            const page = changePagination==="pagination" ? this.page : 1 
+
+            fetch(`http://www.omdbapi.com/?apikey=be1732ea&s=${val}&page=${page}`)
+                .then(res => res.json())
+                .then(res => {
+                    const { totalResults:count, Search:paginatedData } = res
+                    switch(changePagination){
+                        case "autocomplete":{
+                            this.entries = paginatedData
+                            break
+                        }
+                        case "pagination" :{
+                            this.paginatedData = paginatedData.map(e => {
+                                return Object.assign({}, e, {showDetail: false})
+                            })
+                            this.paginationLength = Math.ceil((+count)/this.maxPerPage)
+                            break
+                        }
+                        case "full_search":{
+                            this.page = 1
+                            this.paginatedData = paginatedData.map(e => {
+                                return Object.assign({}, e, {showDetail: false})
+                            })
+                            this.paginationLength = Math.ceil((+count)/this.maxPerPage)
+                        }
+                    }
+                    console.log(count)
+                })
+                .catch(err => {
+                    console.log(err)
+                    this.paginatedData= []
+                    this.paginationLength = 1
+                }).finally(() => {this.paginationLoading = false})
+        },
+        setData(movie=null){
+            const imdbID = movie
+            this.paginationLoading = true
+            fetch(`http://www.omdbapi.com/?apikey=be1732ea&i=${imdbID}` )
+                .then(res => res.json())
+                .then(res => {
+                    Object.keys(res).map((key) => {
+                        if (res[key] === 'N/A' && key !== "Poster") delete res[key]})
+                    Object.assign(res,{})
+                    this.details = Object.assign({}, res)
+                })
+                .catch(err => {
+                    console.log(err)
+                }).finally(() => {this.paginationLoading = false}) 
+        },
+        cardClicked(imdbID, cardIndex) {
+            this.focusedMovie = imdbID
+            this.paginatedData.map((data, i) => {
+                let newEntry = null
+                if(i===cardIndex){
+                    this.detailBlock=!this.paginatedData[cardIndex].showDetail
+                    newEntry = Object.assign({}, data, {showDetail:!this.paginatedData[cardIndex].showDetail})
+                }else{
+                    newEntry = Object.assign({}, data, {showDetail:false})
+                }
+                Vue.set(this.paginatedData, i, newEntry)    
+            })
+        }
     },
 
     watch: {
-      search (val) {
-        // Items have already been loaded
-        //if (this.items.length > 0) return
-
-        // Items have already been requested
-        if (this.isLoading) return
-
-        this.isLoading = true
-
-        // Lazily load input items
-        fetch(`http://www.omdbapi.com/?apikey=be1732ea&s=${val}`)
-          .then(res => res.json())
-          .then(res => {
-            const { totalResults:count, Search:entries } = res
-            //const { count, entries } = res
-            this.count = count
-            this.entries = entries
-            console.log(res)
-          })
-          .catch(err => {
-            console.log(err)
-          })
-          .finally(() => (this.isLoading = false))
-      },
-    },
+        model(){
+            console.log("model watch")
+            this.$refs.combobox.isMenuActive = false
+            this.isLoading = true
+            this.detailBlock = false
+            if(typeof(this.model) === 'object' && this.model !== null){
+                this.detailBlock = true
+                this.setData(this.model.imdbID)
+                this.searchQuery("full_search")
+            }else{
+                this.searchQuery("full_search")
+            }
+            this.isLoading = false
+        },
+        search () {
+            console.log("search watch")
+            if (this.isLoading) return
+            this.isLoading = true
+            this.searchQuery("autocomplete")
+            this.isLoading = false
+        },
+        page (){
+            this.searchQuery("pagination")
+        },
+        focusedMovie(){
+            this.setData(this.focusedMovie)
+    }
+  }
   }
 </script>
 
